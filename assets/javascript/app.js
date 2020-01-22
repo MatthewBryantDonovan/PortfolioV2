b02eeb3a5b72c898ada988d0c711a28670b6b3ba
Matthew Donovan <matthewbryantdonovan@gmail.com>
2020-01-22 01:59:16 -0500

    found a way to have the slick load faster

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,18 +11,70 @@ $(document).ready(function () {
 });
 $(".slick-arrow").attr("id", "slickID");
 
-function portfolio() {
-  $("#bio").css("display", "none");
-  $(".slick-prev").click();
-  $(".carouselSlick").css("display", "inherit");
-  $("#title").css("display", "inherit");
-};
+// //original
+// function portfolio() {
+//   $("#bio").css("display", "none");
+//   $(".slick-prev").click();
+//   $(".carouselSlick").css("display", "inherit");
+//   $("#title").css("display", "inherit");
+// }
+// //original
+// function bio() {
+//   $("#bio").css("display", "inherit");
+//   $(".carouselSlick").css("display", "none");
+//   $("#title").css("display", "none");
+// }
 
+//////////////////////
+//new
+function portfolio() {
+  $("#bio").css({
+    position: "absolute",
+    top: "-99999999px",
+    left: "-9999999999999px"
+  });
+  $(".slick-prev").click();
+$(".carouselSlick").css({
+  position: "inherit",
+  top: "inherit",
+  left: "inherit"
+});
+$("#title").css({
+  position: "initial",
+  top: "inherit",
+  left: "inherit"
+});
+$(".slick-slider").css({
+  position: "sticky"
+});
+$(".slick-dots").css({
+  position: "initial"
+});
+}
+//new
 function bio() {
-  $("#bio").css("display", "inherit");
-  $(".carouselSlick").css("display", "none");
-  $("#title").css("display", "none");
-};
+  $("#bio").css({
+    position: "initial",
+    top: "inherit",
+    left: "inherit"
+  });
+  $(".carouselSlick").css({
+    position: "absolute",
+    top: "-99999999px",
+    left: "-9999999999999px"
+  });
+  $("#title").css({
+    position: "absolute",
+    top: "-99999999px",
+    left: "-9999999999999px"
+  });
+}
+
+/////////////
+
+function CloseSidebar(){
+  $(".sidenav").sidenav('onCloseStart', instance.close());
+}
 
 $('body').on('mouseup', function() {
   console.log( $('.slick-current').attr("data-slick-index") );
@@ -42,4 +94,17 @@ $('body').on('touchend', function() {
   setTimeout(function() { 
     $("#title").html($(".img"+$('.slick-current').attr("data-slick-index")).attr("data-slide"));
 }, 200);
+});
+
+
+///////// trying to get the slick to load faster
+$(".carouselSlick").css({
+  position: "absolute",
+  top: "-99999999px",
+  left: "-9999999999999px"
+});
+$("#title").css({
+  position: "absolute",
+  top: "-99999999px",
+  left: "-9999999999999px"
 });
