87e58a1fecea1c389c2457d694651621d4bec9f0
Matthew Donovan <matthewbryantdonovan@gmail.com>
2020-01-30 16:29:45 -0500

    Added the technologies section to the projects

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,6 @@
+var technology = [];
+var solved = [];
+
 $(document).ready(function () {
   $('.sidenav').sidenav();
   $('.modal').modal();
@@ -93,6 +96,8 @@ $('body').on('mouseup', function() {
   setTimeout(function() { 
     $("#title").html($(".img"+$('.slick-current').attr("data-slick-index")).attr("data-slide"));
     $("#title-link").attr( "href", $(".link"+$('.slick-current').attr("data-slick-index")).attr("href"));
+    $("#technology").html(technology[$('.slick-current').attr("data-slick-index")]);
+    $("#solved").html(solved[$('.slick-current').attr("data-slick-index")]);
 }, 200);
 });
 
@@ -100,12 +105,16 @@ $('body').on('keyup', function() {
   setTimeout(function() { 
     $("#title").html($(".img"+$('.slick-current').attr("data-slick-index")).attr("data-slide"));
     $("#title-link").attr( "href", $(".link"+$('.slick-current').attr("data-slick-index")).attr("href"));
+    $("#technology").html(technology[$('.slick-current').attr("data-slick-index")]);
+    $("#solved").html(solved[$('.slick-current').attr("data-slick-index")]);
 }, 100);
 });
 $('body').on('touchend', function() {
   setTimeout(function() { 
     $("#title").html($(".img"+$('.slick-current').attr("data-slick-index")).attr("data-slide"));
     $("#title-link").attr( "href", $(".link"+$('.slick-current').attr("data-slick-index")).attr("href"));
+    $("#technology").html(technology[$('.slick-current').attr("data-slick-index")]);
+    $("#solved").html(solved[$('.slick-current').attr("data-slick-index")]);
 }, 200);
 });
 
@@ -150,3 +159,56 @@ $("#title").css({
   top: "-99999999px",
   left: "-9999999999999px"
 });
+
+//technology text
+//0
+technology[0] = "";
+//1
+technology[1] = "";
+//2
+technology[2] = "";
+//3
+technology[3] = "Technology highlights: FireBase, jQuery, Bootstrap";
+//4
+technology[4] = "";
+//5
+technology[5] = "Technology highlights: Express, Node.js, MySQL";
+//6
+technology[6] = "";
+//7
+technology[7] = "";
+//8
+technology[8] = "";
+//9
+technology[9] = "";
+//10
+technology[10] = "Technology Highlights: JawsDB, Heroku, Handlebars";
+//11
+technology[11] = "Technology Highlights: Slick.JS, REST, Materialize";
+
+
+//solved text
+//0
+solved[0] = "";
+//1
+solved[1] = "";
+//2
+solved[2] = "";
+//3
+solved[3] = "Play RPS online with the ability to chat and spectate.";
+//4
+solved[4] = "";
+//5
+solved[5] = "Find friends with closest degree of matching survey answers.";
+//6
+solved[6] = "";
+//7
+solved[7] = "";
+//8
+solved[8] = "";
+//9
+solved[9] = "";
+//10
+solved[10] = "Web app to order burgers, reorder, or remove them.";
+//11
+solved[11] = "Aggregation of video game media while keeping a clean UX.";
