22bc30106440a8e768d963bed72e01e285dcf7f6
Matthew Donovan <matthewbryantdonovan@gmail.com>
2020-04-06 11:41:09 -0400

    making it so there are less projects to increase chance of views

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -193,70 +193,96 @@ $("#solved").css({
 });
 
 //technology text
-//0 - giphyMatt
-technology[15] = "Technology highlights: AJAX, User Favorites, Bootstrap";
-//1 - Castlevania: Halls of Supremacy
-technology[14] = "Technology highlights: Click Events, jQuery, Bootstrap";
-//2 - John Wick Trivia
-technology[13] = "Technology highlights: Timers, Click Events, Object Methods";
+// //0 - giphyMatt
+// technology[15] = "Technology highlights: AJAX, User Favorites, Bootstrap";
+// //1 - Castlevania: Halls of Supremacy
+// technology[14] = "Technology highlights: Click Events, jQuery, Bootstrap";
+// //2 - John Wick Trivia
+// technology[13] = "Technology highlights: Timers, Click Events, Object Methods";
+// //3 - Rock Paper Scissors Online
+// technology[12] = "Technology highlights: FireBase, jQuery, Bootstrap";
+// //4 - Metal Gear Hangman
+// technology[11] = "Technology highlights: Responsive Design, Click Events, JavaScript";
+// //5 - Friend Finder
+// technology[10] = "Technology highlights: Express, Node.js, MySQL";
+// //6 - Liri Bot
+// technology[9] = "Technology highlights: Axios, Node.js, CLI";
+// //7 - Word Guess CLI
+// technology[8] = "Technology highlights: Constructors, Node.js, CLI";
+// //8 - Bamazon CLI
+// technology[7] = "Technology highlights: MySQL, Node.js, CLI";
+// //9 - Stardate API
+// technology[6] = "Technology highlights: Restify, API, Moment.JS";
+// //10 - Bob's Burgers!
+// technology[5] = "Technology Highlights: JawsDB, Heroku, Handlebars";
+// //11 - Up Game News!
+// technology[4] = "Technology Highlights: mongoose, mongoDB, cheerio";
+// //12 - Super Clicky Bros
+// technology[3] = "Technology Highlights: React.JS, Materialize, React Components";
+// //14 - Matt Books Pro
+// technology[2] = "Technology Highlights: React.JS, React-Router, Materialize";
+// //13 - Videogame Hypeman
+// technology[1] = "Technology Highlights: Vue.JS, Slick.JS, Materialize";
+// //13 - Clip a la Twitch
+// technology[0] = "Technology Highlights: React-hooks, Passport-Local, Mongoose";
+
+
+// //solved text
+// //0 - giphyMatt
+// solved[15] = "Find gifs of your favorite animals.";
+// //1 - Castlevania: Halls of Supremacy
+// solved[14] = "Play a browser based Role Playing Game with multiple paths to victory.";
+// //2 - John Wick Trivia
+// solved[13] = "Guess John Wick movie based questions correctly in this timed trivia game.";
+// //3 - Rock Paper Scissors Online
+// solved[12] = "Play RPS online with the ability to chat and spectate.";
+// //4 - Metal Gear Hangman
+// solved[11] = "Browser based hangman game with great audio.";
+// //5 - Friend Finder
+// solved[10] = "Find friends with closest degree of matching survey answers.";
+// //6 - Liri Bot
+// solved[9] = "This bot will will take commands to show concert, song, and move data.";
+// //7 - Word Guess CLI
+// solved[8] = "Play a hangman game with a Command Line interface.";
+// //8 - Bamazon CLI
+// solved[7] = "Command Line interface that allows customers to buy items from a MySQL database.";
+// //9 - Stardate API
+// solved[6] = "Convert dates into Star Trek stardates or Star Trek stardates into dates.";
+// //10 - Bob's Burgers!
+// solved[5] = "Web app to order burgers, reorder, or remove them.";
+// //12 - Up Game News!
+// solved[4] = "Get freshly scraped GameSpot news for games, movies, and more!";
+// //12 - Super Clicky Bros
+// solved[3] = "Memory game in which you click every character once, if you click them twice the game resets.";
+// //14 - Matt Books Pro
+// solved[2] = "Matt Books Pro allows users to save entries from google books to later view or buy them.";
+// //13 - Videogame Hypeman
+// solved[1] = "Aggregation of video game media while keeping a clean UX.";
+// //13 - Clip a la Twitch
+// solved[0] = "Social media site to view clips from Twitch.tv.";
+
+// TODO: These are the minimum projects
+
 //3 - Rock Paper Scissors Online
-technology[12] = "Technology highlights: FireBase, jQuery, Bootstrap";
+technology[4] = "Technology highlights: FireBase, jQuery, Bootstrap";
 //4 - Metal Gear Hangman
-technology[11] = "Technology highlights: Responsive Design, Click Events, JavaScript";
-//5 - Friend Finder
-technology[10] = "Technology highlights: Express, Node.js, MySQL";
-//6 - Liri Bot
-technology[9] = "Technology highlights: Axios, Node.js, CLI";
-//7 - Word Guess CLI
-technology[8] = "Technology highlights: Constructors, Node.js, CLI";
-//8 - Bamazon CLI
-technology[7] = "Technology highlights: MySQL, Node.js, CLI";
+technology[3] = "Technology highlights: Responsive Design, Click Events, JavaScript";
 //9 - Stardate API
-technology[6] = "Technology highlights: Restify, API, Moment.JS";
-//10 - Bob's Burgers!
-technology[5] = "Technology Highlights: JawsDB, Heroku, Handlebars";
-//11 - Up Game News!
-technology[4] = "Technology Highlights: mongoose, mongoDB, cheerio";
-//12 - Super Clicky Bros
-technology[3] = "Technology Highlights: React.JS, Materialize, React Components";
-//14 - Matt Books Pro
-technology[2] = "Technology Highlights: React.JS, React-Router, Materialize";
+technology[2] = "Technology highlights: Restify, API, Moment.JS";
 //13 - Videogame Hypeman
 technology[1] = "Technology Highlights: Vue.JS, Slick.JS, Materialize";
 //13 - Clip a la Twitch
 technology[0] = "Technology Highlights: React-hooks, Passport-Local, Mongoose";
 
-
-//solved text
-//0 - giphyMatt
-solved[15] = "Find gifs of your favorite animals.";
-//1 - Castlevania: Halls of Supremacy
-solved[14] = "Play a browser based Role Playing Game with multiple paths to victory.";
-//2 - John Wick Trivia
-solved[13] = "Guess John Wick movie based questions correctly in this timed trivia game.";
 //3 - Rock Paper Scissors Online
-solved[12] = "Play RPS online with the ability to chat and spectate.";
+solved[4] = "Play RPS online with the ability to chat and spectate.";
 //4 - Metal Gear Hangman
-solved[11] = "Browser based hangman game with great audio.";
-//5 - Friend Finder
-solved[10] = "Find friends with closest degree of matching survey answers.";
-//6 - Liri Bot
-solved[9] = "This bot will will take commands to show concert, song, and move data.";
-//7 - Word Guess CLI
-solved[8] = "Play a hangman game with a Command Line interface.";
-//8 - Bamazon CLI
-solved[7] = "Command Line interface that allows customers to buy items from a MySQL database.";
+solved[3] = "Browser based hangman game with great audio.";
 //9 - Stardate API
-solved[6] = "Convert dates into Star Trek stardates or Star Trek stardates into dates.";
-//10 - Bob's Burgers!
-solved[5] = "Web app to order burgers, reorder, or remove them.";
-//12 - Up Game News!
-solved[4] = "Get freshly scraped GameSpot news for games, movies, and more!";
-//12 - Super Clicky Bros
-solved[3] = "Memory game in which you click every character once, if you click them twice the game resets.";
-//14 - Matt Books Pro
-solved[2] = "Matt Books Pro allows users to save entries from google books to later view or buy them.";
+solved[2] = "Convert dates into Star Trek stardates or Star Trek stardates into dates.";
 //13 - Videogame Hypeman
 solved[1] = "Aggregation of video game media while keeping a clean UX.";
 //13 - Clip a la Twitch
 solved[0] = "Social media site to view clips from Twitch.tv.";
+
+// TODO: These are the minimum projects
